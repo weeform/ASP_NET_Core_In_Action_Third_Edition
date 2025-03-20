6c4d944a146e91bde780ffb5433b89153ce903fb
weeform <lihuifeng01@hotmail.com>
2025-03-20 16:36:53 +0800

    Steps to create and start a dotnet Web application

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,15 @@
+// Create a WebAPplicationBuilder instance
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the required services and configuration with the WebApplicationBuilder
+
+// Call Build() on the builder instance to create a WebApplication instance
 var app = builder.Build();
 
+// Add middleware to the WebApplication to create a pipeline
+
+// Map the endpoints to the WebApplication
 app.MapGet("/", () => "Hello World!");
 
+// Call Run() on the WebApplication to start the server and handle requests
 app.Run();
